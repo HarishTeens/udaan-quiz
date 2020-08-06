f53672ea06d779cb2cd939797dd41d16df793ddc
Harish <arishh2@gmail.com>
2020-08-06 10:04:24 +0530

    first working prototype

---
+++ app.js
@@ -0,0 +1,78 @@
+const express=require('express')();
+const questions=require('./questions.json');
+const bodyparser=require('body-parser');
+
+
+express.set("view engine", "ejs");
+express.use(bodyparser.urlencoded({ extended: true }));
+let scores=[]
+let player
+
+express.get("/",(req,res)=>{
+    res.render("index")
+})
+
+express.post("/",(req,res)=>{
+    player=req.body.name;
+    res.redirect("/courses");
+})
+
+isName=(req,res,next)=>{
+    res.statusCode=200;
+    if(player){
+        next();
+    }
+    res.redirect("/");
+}
+
+express.get("/courses",isName,(req,res)=>{
+    courses=[]
+    Object.keys(questions).forEach(question=>{
+        courses.push(question);
+    })
+    res.render("courses",{courses:courses});
+})
+
+
+express.get("/course/:name",isName,(req,res)=>{
+    const courseName=req.params.name;
+    const quiz=questions[courseName];
+    res.render("quiz",{quiz:quiz,courseName:courseName});
+})
+
+express.post("/course/:name",(req,res)=>{
+    const responses=req.body;
+    const courseName=req.params.name;
+    const courseQuestions=questions[courseName];
+    let score=0;
+    Object.keys(responses).forEach((response)=>{
+        const questionIndex=parseInt(response.substring(1));
+        console.log(responses[response]);
+        if(responses[response]==courseQuestions[questionIndex].answer)
+            score=score+1;
+    })
+    const scoreRecord={
+        name:player,
+        score:score,
+        course:courseName,
+        time:Date()
+    };
+    
+    if(! (player in scores)){
+        scores[player]=[];
+        scores[player].push(scoreRecord);
+    }else{
+        scores[player].push(scoreRecord);
+    }
+    res.redirect("/score/"+player);
+})
+
+express.get("/score/:name",isName,(req,res)=>{
+    res.send(scores[req.params.name]);
+})
+
+
+
+express.listen(3000,()=>{
+    console.log('Server started!');
+})
